12f2ac91887d42a0a168b6677dc0409820736f08
Yonatan <yvalenta@gmail.com>
2014-09-06 07:01:49 -0500

    espect

--- spec/models/empresa_spec.rb
+++ spec/models/empresa_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 describe Empresa do
-  it 'Es valido con nombre y ciudad'
-  it 'Es invalido sin nombre'
-  it 'Es invalido sin ciudad'
-  it 'Debe retornar 10 veces el nombre'
+  it 'Es valido con nombre y ciudad' do
+    expect(true).to be true
+  end
+  it 'Es invalido sin nombre' do
+    expect(true).to be true
+  end
+  it 'Es invalido sin ciudad' do
+    expect(true).to be true
+  end
+  it 'Debe retornar 10 veces el nombre' do
+    expect(true).to be true
+  end
 end
