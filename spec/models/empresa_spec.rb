466b6bb589437991ee7a100827fa890d41b3652a
Yonatan <yvalenta@gmail.com>
2014-09-06 07:42:18 -0500

    Adicionando prueba metodos de instancia

--- spec/models/empresa_spec.rb
+++ spec/models/empresa_spec.rb
@@ -1,31 +1,43 @@
 require 'rails_helper'
 
 describe Empresa do
-  it 'Es valido con nombre y ciudad' do
-    empresa = Empresa.new( nombre: 'Empresa del Perro',
-                           departamento_id: 1,
-                           ciudad_id: 1)
-    expect(empresa).to be_valid
+  describe 'validaciones' do
+
+    it 'Es valido con nombre y ciudad' do
+      empresa = Empresa.new( nombre: 'Empresa del Perro',
+                             departamento_id: 1,
+                             ciudad_id: 1)
+      expect(empresa).to be_valid
+    end
+    it 'Es invalido sin nombre' do
+      empresa = Empresa.new( nombre:nil )
+      empresa.valid?
+      p empresa.errors[:nombre]
+      expect(empresa.errors[:nombre].count).to eq 1
+    end
+    it 'Es invalido sin ciudad' do
+      empresa = Empresa.new( ciudad_id:nil)
+      empresa.valid?
+      p empresa.errors[:ciudad_id]
+      expect(empresa.errors[:ciudad_id].count).to eq 1
+    end
+    it 'Es invalido sin departamento' do
+      empresa = Empresa.new( departamento_id:nil)
+      empresa.valid?
+      p empresa.errors[:departamento_id]
+      expect(empresa.errors[:departamento_id].count).to eq 1
+    end
+
   end
-  it 'Es invalido sin nombre' do
-    empresa = Empresa.new( nombre:nil )
-    empresa.valid?
-    p empresa.errors[:nombre]
-    expect(empresa.errors[:nombre].count).to eq 1
+
+  describe 'Metodos de instancia' do
+    it 'Debe retornar 10 veces el nombre' do
+      expect(true).to be true
+    end
   end
-  it 'Es invalido sin ciudad' do
-    empresa = Empresa.new( ciudad_id:nil)
-    empresa.valid?
-    p empresa.errors[:ciudad_id]
-    expect(empresa.errors[:ciudad_id].count).to eq 1
+
+  describe 'Metodos de clase' do
+
   end
-  it 'Es invalido sin departamento' do
-    empresa = Empresa.new( departamento_id:nil)
-    empresa.valid?
-    p empresa.errors[:departamento_id]
-    expect(empresa.errors[:departamento_id].count).to eq 1
-  end
-  it 'Debe retornar 10 veces el nombre' do
-    expect(true).to be true
-  end
+
 end
