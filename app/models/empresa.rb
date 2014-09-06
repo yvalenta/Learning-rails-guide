80eae49c061350603035869b783760ca87b7572e
Yonatan <yvalenta@gmail.com>
2014-09-06 06:58:19 -0500

    Cambiando agencia por empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,4 +1,8 @@
 class Empresa < ActiveRecord::Base
   belongs_to :ciudad
   belongs_to :departamento
+
+  def nombre_largo
+    'oe' * 10
+  end
 end
---
+++ spec/models/empresa_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Empresa do
+  it 'Es valido con nombre y ciudad'
+  it 'Es invalido sin nombre'
+  it 'Es invalido sin ciudad'
+  it 'Debe retornar 10 veces el nombre'
+end
