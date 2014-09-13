11daea0d4a1fb9e6380101bccaa2a33cf51e41f4
Yonatan <yvalenta@gmail.com>
2014-09-13 06:32:24 -0500

    inicial test_controller

--- spec/controllers/empresa_controller_spec.rb
+++ spec/controllers/empresa_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-describe 'My beavior' do
-  it 'should do something' do
-    true.should == false
-  end
+describe EmpresasController do
+
+  describe 'get #index'
+  describe 'get #show'
+  describe 'get #edit'
+  describe 'post #create'
+  describe 'patch #update'
+  describe 'destroy #destroy'
+
 end
